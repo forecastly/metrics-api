df10653649c42230a6dd0727beeb7fcedc277bf8
Bill Franklin <bilbof@users.noreply.github.com>
2015-11-05 14:45:10 +0000

    Update chartmogul.py

--- Examples/geckoboard-integration/chartmogul.py
+++ Examples/geckoboard-integration/chartmogul.py
@@ -1,3 +1,21 @@
+#    Extremely simple ChatMogul read-only API wrapper
+#    https://github.com/bencer/python-chartmogul
+#    Copyright (C) 2015 Jorge Salamero <bencer@cauterized.net>
+#
+#    Authors: Jorge Salamero <bencer@cauterized.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import urllib.request
 import ssl
 import base64
